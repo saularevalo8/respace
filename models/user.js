5d2620705b1bde07e74ca79ef25cacc72c2df147
Jordan Brown <jordan@Jordans-MacBook-Air.local>
2017-05-15 18:10:26 -0700

    routing

--- models/user.js
+++ models/user.js
@@ -1,18 +1,32 @@
 'use strict';
 module.exports = function(sequelize, DataTypes) {
-  var User = sequelize.define('User', {
-    firstName: DataTypes.STRING,
-    lastName: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING
-  }, {
-    classMethods: {
-      associate: function(models) {
-        // associations can be defined here
-        User.hasMany(models.Location)
-        User.hasMany(models.Transaction);
-      }
-    }
+  var User = sequelize.define("User", {
+  //   firstName: DataTypes.STRING,
+  //   lastName: DataTypes.STRING,
+  //   email: DataTypes.STRING,
+  //   password: DataTypes.STRING
+  // }
+  firstName: {
+      type: DataTypes.STRING
+    },
+    lastName: {
+      type: DataTypes.STRING
+    },
+    email: {
+      type: DataTypes.STRING
+     },
+     password: {
+      type: DataTypes.STRING
+     }
+     
+  // , {
+  //   classMethods: {
+  //     associate: function(models) {
+  //       // associations can be defined here
+  //       User.hasMany(models.Location)
+  //       User.hasMany(models.Transaction);
+  //     }
+  //   }
   });
   return User;
 };
--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.static("public"));
 
 // Requiring our routes
  require("./routes/html-routes.js")(app);
-//require("./routes/api-routes.js")(app);
+require("./routes/api-routes.js")(app);
 
 // Syncing our database and logging a message to the user upon success
 db.sequelize.sync({force:true}).then(function() {
