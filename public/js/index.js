8cefd3aa62af7841707ed6f448f7a50714838452
SaulValo <SaulValo@MacBook-Pro-2.local>
2017-05-17 06:22:44 -0700

    updated bcrypt functionality

--- public/js/index.js
+++ public/js/index.js
@@ -1,35 +1,38 @@
 $(document).ready(function() {
-	var firstInput = $("#firstName_prefix");
-  var lastInput = $("#lastName_prefix");
-  var usernameInput = $("#username");
-  var passwordInput = $("#password");
+    var firstInput = $("#firstName_prefix");
+    var lastInput = $("#lastName_prefix");
+    var usernameInput = $("#username");
+    var passwordInput = $("#password");
 
-  $("#create").on("click", function() {
-  	event.preventDefault();
-    // Wont submit the post if we are missing a body or a title
-    if (!firstInput.val().trim() || !lastInput.val().trim()) {
-      return;
-    }
-    // Constructing a newPost object to hand to the database
-    var newUser = {
-      firstName: firstInput.val().trim(),
-      lastName: lastInput.val().trim(),
-      email: usernameInput.val().trim(),
-      password: passwordInput.val().trim()
+    $("#create").on("click", function() {
+        event.preventDefault();
+        // Wont submit the post if we are missing a body or a title
+        if (!firstInput.val().trim() || !lastInput.val().trim()) {
+            return;
+        }
+        // Constructing a newPost object to hand to the database
+        var newUser = {
+            firstName: firstInput.val().trim(),
+            lastName: lastInput.val().trim(),
+            email: usernameInput.val().trim(),
+            password: passwordInput.val().trim()
+        };
+
+        if (!newUser.firstName || !newUser.lastName || !newUser.email || !newUser.password) {
+            return alert("Please don't leave fields blank");
+        }
+        console.log(newUser);
+        if (newUser) {
+            submitUser(newUser);
+
+        }
+
+    });
+
+    function submitUser(User) {
+        $.post("/api/users/", User, function() {
+            window.location.href = "/profile";
+        });
     };
 
-    console.log(newUser);
-if (newUser) {
-     submitUser(newUser);
-
-    }
-    
-  	});
-
-  	function submitUser(User) {
-    $.post("/api/users/", User, function() {
-      window.location.href = "/profile";
-    }); 
-  	};
-
-  	});
+});
