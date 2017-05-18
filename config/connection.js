c23c9d9620c2cde8d60fdf4472fa8992ef403a60
Blake Nelson <Blake Nelson>
2017-05-17 21:25:21 -0700

    HTML/CSS updates

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
-// var Sequelize = require("sequelize");
+var Sequelize = require("sequelize");
 
-<<<<<<< HEAD
-var sequelize = new Sequelize("respace_db", "root", "", {
+
+var sequelize = new Sequelize("respace_db", "root", "F0ll0wthewhiterabbit", {
 	host: "localhost",
 	dialect: "mysql",
 	pool: {
@@ -10,7 +10,7 @@ var sequelize = new Sequelize("respace_db", "root", "", {
 		idle: 10000
 	}
 });
-=======
+
 // var sequelize = new Sequelize("respace_db", "root", "" {
 // 	host: "localhost",
 // 	dialect: "mysql",
@@ -20,6 +20,5 @@ var sequelize = new Sequelize("respace_db", "root", "", {
 // 		idle: 10000
 // 	}
 // });
->>>>>>> ae2dfd6f9859b9b5faa96c14828f042824715f98
 
-// module.exports = sequelize;
+module.exports = sequelize;
