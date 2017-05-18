cd34d9bc414574bc3d5118293995cd550251e413
Thomas Carpenter <thomascarpenter@Thomass-MacBook-Pro.local>
2017-05-17 21:15:09 -0700

    end of class changes

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,7 @@
 // var Sequelize = require("sequelize");
 
-<<<<<<< HEAD
-var sequelize = new Sequelize("respace_db", "root", "", {
-	host: "localhost",
-	dialect: "mysql",
-	pool: {
-		max: 5,
-		min: 0,
-		idle: 10000
-	}
-});
-=======
-// var sequelize = new Sequelize("respace_db", "root", "" {
+
+// var sequelize = new Sequelize("respace_db", "root", "", {
 // 	host: "localhost",
 // 	dialect: "mysql",
 // 	pool: {
@@ -20,6 +10,15 @@ var sequelize = new Sequelize("respace_db", "root", "", {
 // 		idle: 10000
 // 	}
 // });
->>>>>>> ae2dfd6f9859b9b5faa96c14828f042824715f98
 
-// module.exports = sequelize;
+// // var sequelize = new Sequelize("respace_db", "root", "" {
+// // 	host: "localhost",
+// // 	dialect: "mysql",
+// // 	pool: {
+// // 		max: 5,
+// // 		min: 0,
+// // 		idle: 10000
+// // 	}
+// // });
+
+// // module.exports = sequelize;
