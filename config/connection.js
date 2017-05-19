6ff102e2c1bc87c88feef49ce8cca9edf1353d86
Jordan Brown <jordan@Jordans-MacBook-Air.local>
2017-05-18 17:37:18 -0700

    okok

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,20 @@
-// var Sequelize = require("sequelize");
+// // var Sequelize = require("sequelize");
 
-<<<<<<< HEAD
-// var sequelize = new Sequelize("respace_db", "root", "password" {
+// <<<<<<< HEAD
+// // var sequelize = new Sequelize("respace_db", "root", "password" {
+// // 	host: "localhost",
+// // 	dialect: "mysql",
+// // 	pool: {
+// // 		max: 5,
+// // 		min: 0,
+// // 		idle: 10000
+// // 	}
+// // });
+
+// // module.exports = sequelize;
+// =======
+
+// var sequelize = new Sequelize("respace_db", "root", "", {
 // 	host: "localhost",
 // 	dialect: "mysql",
 // 	pool: {
@@ -10,29 +23,16 @@
 // 		idle: 10000
 // 	}
 // });
+
+// // var sequelize = new Sequelize("respace_db", "root", "" {
+// // 	host: "localhost",
+// // 	dialect: "mysql",
+// // 	pool: {
+// // 		max: 5,
+// // 		min: 0,
+// // 		idle: 10000
+// // 	}
+// // });
 
 // module.exports = sequelize;
-=======
-
-var sequelize = new Sequelize("respace_db", "root", "", {
-	host: "localhost",
-	dialect: "mysql",
-	pool: {
-		max: 5,
-		min: 0,
-		idle: 10000
-	}
-});
-
-// var sequelize = new Sequelize("respace_db", "root", "" {
-// 	host: "localhost",
-// 	dialect: "mysql",
-// 	pool: {
-// 		max: 5,
-// 		min: 0,
-// 		idle: 10000
-// 	}
-// });
-
-module.exports = sequelize;
->>>>>>> a2ab2311b51c2427abb14e4f8955fba05d3c273a
+// >>>>>>> a2ab2311b51c2427abb14e4f8955fba05d3c273a
